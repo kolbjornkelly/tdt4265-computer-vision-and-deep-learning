2a running, but the gradient is incorrect

assignment1/task2a.py
@@ -12,7 +12,10 @@ def pre_process_images(X: np.ndarray):
     """
     assert X.shape[1] == 784,\
         f"X.shape[1]: {X.shape[1]}, should be 784"
-    # TODO implement this function (Task 2a)
+
+    X = X / 127.5 - 1  # Normalize to [0,2], then subtract 1
+    bias_trick = np.zeros(shape=(X.shape[0], 1)) + 1
+    X = np.append(X, bias_trick, axis=1)
     return X
 
 
@@ -24,17 +27,24 @@ def cross_entropy_loss(targets: np.ndarray, outputs: np.ndarray) -> float:
     Returns:
         Cross entropy error (float)
     """
-    # TODO implement this function (Task 2a)
+    #C = -np.multiply(self.targets, np.log(self.y)) + (1 - self.y)
+    cross_entropies = -targets * \
+        np.log(outputs) + (1 - targets) * np.log(1 - outputs)
+
+    cross_entropy_error = np.average(cross_entropies)
+
     assert targets.shape == outputs.shape,\
         f"Targets shape: {targets.shape}, outputs: {outputs.shape}"
-    return 0
+
+    print("Cross entropy error: ", cross_entropy_error)
+    return cross_entropy_error
 
 
 class BinaryModel:
 
     def __init__(self):
         # Define number of input nodes
-        self.I = None
+        self.I = 785  # This is bad -> change later
         self.w = np.zeros((self.I, 1))
         self.grad = None
 
@@ -45,8 +55,9 @@ class BinaryModel:
         Returns:
             y: output of model with shape [batch size, 1]
         """
-        # TODO implement this function (Task 2a)
-        return None
+
+        y = 1 / (1 + np.exp(-X.dot(self.w)))
+        return y
 
     def backward(self, X: np.ndarray, outputs: np.ndarray, targets: np.ndarray) -> None:
         """
@@ -56,10 +67,14 @@ class BinaryModel:
             outputs: outputs of model of shape: [batch size, 1]
             targets: labels/targets of each image of shape: [batch size, 1]
         """
-        # TODO implement this function (Task 2a)
+
+        grads = -(targets - outputs) * X
+        avg_grads = np.average(grads, 0)
+        self.grad = avg_grads.reshape(self.w.shape[0], 1) / targets.shape[0]
+        print("Grad shape: ", self.grad.shape)
         assert targets.shape == outputs.shape,\
             f"Output shape: {outputs.shape}, targets: {targets.shape}"
-        self.grad = np.zeros_like(self.w)
+        #self.grad = np.zeros_like(self.w)
         assert self.grad.shape == self.w.shape,\
             f"Grad shape: {self.grad.shape}, w: {self.w.shape}"
 
@@ -72,7 +87,8 @@ def gradient_approximation_test(model: BinaryModel, X: np.ndarray, Y: np.ndarray
         Numerical approximation for gradients. Should not be edited. 
         Details about this test is given in the appendix in the assignment.
     """
-    w_orig = np.random.normal(loc=0, scale=1/model.w.shape[0]**2, size=model.w.shape)
+    w_orig = np.random.normal(
+        loc=0, scale=1/model.w.shape[0]**2, size=model.w.shape)
     epsilon = 1e-3
     for i in range(w_orig.shape[0]):
         model.w = w_orig.copy()
@@ -89,6 +105,7 @@ def gradient_approximation_test(model: BinaryModel, X: np.ndarray, Y: np.ndarray
         logits = model.forward(X)
         model.backward(X, logits, Y)
         difference = gradient_approximation - model.grad[i, 0]
+        #print("Difference: ", abs(difference))
         assert abs(difference) <= epsilon**2,\
             f"Calculated gradient is incorrect. " \
             f"Approximation: {gradient_approximation}, actual gradient: {model.grad[i,0]}\n" \
@@ -100,8 +117,10 @@ if __name__ == "__main__":
     category1, category2 = 2, 3
     X_train, Y_train, *_ = utils.load_binary_dataset(category1, category2)
     X_train = pre_process_images(X_train)
-    assert X_train.max() <= 1.0, f"The images (X_train) should be normalized to the range [-1, 1]"
-    assert X_train.min() < 0 and X_train.min() >= -1, f"The images (X_train) should be normalized to the range [-1, 1]"
+    assert X_train.max(
+    ) <= 1.0, f"The images (X_train) should be normalized to the range [-1, 1]"
+    assert X_train.min() < 0 and X_train.min() >= - \
+        1, f"The images (X_train) should be normalized to the range [-1, 1]"
     assert X_train.shape[1] == 785,\
         f"Expected X_train to have 785 elements per image. Shape was: {X_train.shape}"
 