065a0abb73ba76710a814a5b2917b94b7a78749b
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-03-02 11:28:54 +0100

    modularize task3

--- assignment3/task3.py
+++ assignment3/task3.py
@@ -1,31 +1,193 @@
-import importlib
+import pathlib
+import matplotlib.pyplot as plt
+import utils
 import torch
-import torch.nn as nn
+from torch import nn
 from dataloaders import load_cifar10
-import numpy as np
-
 from trainer import Trainer, compute_loss_and_accuracy
-from task2 import create_plots, ExampleModel
 
 
-def run():
-    torch.multiprocessing.freeze_support()
+class ModelX(nn.Module):
+
+    def __init__(self,
+                 image_channels,
+                 num_classes):
+        """
+            Is called when model is initialized.
+            Args:
+                image_channels. Number of color channels in image (3)
+                num_classes: Number of classes we want to predict (10)
+        """
+        super().__init__()
+        # TODO: Implement this function (Task  2a)
+
+        self.hidden_layer_units = 64
+        num_filters = 32  # Set number of filters in first conv layer
+        self.num_classes = num_classes
+        # Define the convolutional layers
+        self.feature_extractor = nn.Sequential(
+            # First layer
+            nn.Conv2d(
+                in_channels=image_channels,
+                out_channels=num_filters,
+                kernel_size=5,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2
+            ),
+            # Second Layer
+            nn.Conv2d(
+                in_channels=num_filters,
+                out_channels=2*num_filters,
+                kernel_size=5,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2
+            ),
+            # Third Layer
+            nn.Conv2d(
+                in_channels=2*num_filters,
+                out_channels=4*num_filters,
+                kernel_size=5,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2
+            ),
+        )
+        # The output of feature_extractor will be [batch_size, num_filters, 16, 16]
+        self.num_output_features = 4 * num_filters * 4 * 4
+        # Initialize our last fully connected layer
+        # Inputs all extracted features from the convolutional layers
+        # Outputs num_classes predictions, 1 for each class.
+        # There is no need for softmax activation function, as this is
+        # included with nn.CrossEntropyLoss
+        self.classifier = nn.Sequential(
+            nn.Linear(self.num_output_features, self.hidden_layer_units),
+            nn.ReLU(),
+            nn.Linear(self.hidden_layer_units, num_classes),
+        )
+
+    def forward(self, x):
+        """
+        Performs a forward pass through the model
+        Args:
+            x: Input image, shape: [batch_size, 3, 32, 32]
+        """
+
+        batch_size = x.shape[0]
+
+        features = self.feature_extractor(x)
+        # Flatten before classification layer
+        features = torch.reshape(
+            features, (x.shape[0], self.num_output_features))
+        out = self.classifier(features)
+
+        expected_shape = (batch_size, self.num_classes)
+        assert out.shape == (batch_size, self.num_classes),\
+            f"Expected output of forward pass to be: {expected_shape}, but got: {out.shape}"
+        return out
 
 
-if __name__ == '__main__':
-    run()
-    epochs = 1
-    batch_size = 64
-    learning_rate = 1e-2  # Should be 5e-5 for LeNet
-    early_stop_count = 4
-    dataloaders = load_cifar10(batch_size)
-    model = ExampleModel(image_channels=3, num_classes=10)
-    trainer = Trainer(
-        batch_size,
-        learning_rate,
-        early_stop_count,
-        epochs,
-        model,
-        dataloaders
-    )
-    trainer.train()
+class ModelY(nn.Module):
+
+    def __init__(self,
+                 image_channels,
+                 num_classes):
+        """
+            Is called when model is initialized.
+            Args:
+                image_channels. Number of color channels in image (3)
+                num_classes: Number of classes we want to predict (10)
+        """
+        super().__init__()
+        # TODO: Implement this function (Task  2a)
+
+        self.hidden_layer_units = 64
+        num_filters = 32  # Set number of filters in first conv layer
+        self.num_classes = num_classes
+        # Define the convolutional layers
+        self.feature_extractor = nn.Sequential(
+            # First layer
+            nn.Conv2d(
+                in_channels=image_channels,
+                out_channels=num_filters,
+                kernel_size=5,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2
+            ),
+            # Second Layer
+            nn.Conv2d(
+                in_channels=num_filters,
+                out_channels=2*num_filters,
+                kernel_size=5,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2
+            ),
+            # Third Layer
+            nn.Conv2d(
+                in_channels=2*num_filters,
+                out_channels=4*num_filters,
+                kernel_size=5,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2
+            ),
+        )
+        # The output of feature_extractor will be [batch_size, num_filters, 16, 16]
+        self.num_output_features = 4 * num_filters * 4 * 4
+        # Initialize our last fully connected layer
+        # Inputs all extracted features from the convolutional layers
+        # Outputs num_classes predictions, 1 for each class.
+        # There is no need for softmax activation function, as this is
+        # included with nn.CrossEntropyLoss
+        self.classifier = nn.Sequential(
+            nn.Linear(self.num_output_features, self.hidden_layer_units),
+            nn.ReLU(),
+            nn.Linear(self.hidden_layer_units, num_classes),
+        )
+
+    def forward(self, x):
+        """
+        Performs a forward pass through the model
+        Args:
+            x: Input image, shape: [batch_size, 3, 32, 32]
+        """
+
+        batch_size = x.shape[0]
+
+        features = self.feature_extractor(x)
+        # Flatten before classification layer
+        features = torch.reshape(
+            features, (x.shape[0], self.num_output_features))
+        out = self.classifier(features)
+
+        expected_shape = (batch_size, self.num_classes)
+        assert out.shape == (batch_size, self.num_classes),\
+            f"Expected output of forward pass to be: {expected_shape}, but got: {out.shape}"
+        return out
--- assignment3/task3_trainer.py
+++ assignment3/task3_trainer.py
@@ -5,7 +5,8 @@ from dataloaders import load_cifar10
 import numpy as np
 
 from trainer import Trainer, compute_loss_and_accuracy
-from task2 import create_plots, ExampleModel
+from task2 import create_plots
+from task3 import ModelX, ModelY
 
 
 def run():
@@ -19,13 +20,26 @@ if __name__ == '__main__':
     learning_rate = 1e-2  # Should be 5e-5 for LeNet
     early_stop_count = 4
     dataloaders = load_cifar10(batch_size)
-    model = ExampleModel(image_channels=3, num_classes=10)
-    trainer = Trainer(
+
+    model_x = ModelX(image_channels=3, num_classes=10)
+    trainer_x = Trainer(
         batch_size,
         learning_rate,
         early_stop_count,
         epochs,
-        model,
+        model_x,
         dataloaders
     )
-    trainer.train()
+
+    model_y = ModelY(image_channels=3, num_classes=10)
+    trainer_y = Trainer(
+        batch_size,
+        learning_rate,
+        early_stop_count,
+        epochs,
+        model_y,
+        dataloaders
+    )
+
+    trainer_x.train()
+    trainer_y.train()
