27772088636a28844fee992adea237736f3deab8
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-03-04 11:25:01 +0100

    4a done

---
+++ assignment3/dataloaders_task4.py
@@ -0,0 +1,90 @@
+from torchvision import transforms, datasets
+from torch.utils.data.sampler import SubsetRandomSampler
+import torch
+import typing
+import numpy as np
+np.random.seed(0)
+
+mean = (0.5, 0.5, 0.5)
+std = (.25, .25, .25)
+
+
+def load_cifar10(batch_size: int, validation_fraction: float = 0.1, trans_opt: str = '') -> typing.List[torch.utils.data.DataLoader]:
+    # Note that transform train will apply the same transform for
+    # validation!
+    print("Loading Images for Task 4")
+    transform_train = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225]),
+        transforms.Resize((224, 224))
+    ])
+
+    data_train = datasets.CIFAR10('data/cifar10',
+                                  train=True,
+                                  download=True,
+                                  transform=transform_train)
+
+    # Define transforms
+    p = 0.5
+    x_transforms = [transforms.RandomCrop((224, 224)),
+                    transforms.RandomHorizontalFlip(p),
+                    transforms.ColorJitter(),
+                    transforms.RandomGrayscale(p)]
+    transform_train_x = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225]),
+        # TODO: reszise before or after normalizing?
+        transforms.Resize((224, 224)),
+        transforms.RandomApply(x_transforms, p)
+    ])
+
+    # Apply transforms
+    data_train_x = datasets.CIFAR10('data/cifar10',
+                                    train=True,
+                                    download=True,
+                                    transform=transform_train_x)
+
+    # Concatenate with base data
+    data_train = torch.utils.data.ConcatDataset(
+        (data_train, data_train_x))
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225]),
+        transforms.Resize((224, 224))
+    ])
+
+    data_test = datasets.CIFAR10('data/cifar10',
+                                 train=False,
+                                 download=True,
+                                 transform=transform_test)
+
+    indices = list(range(len(data_train)))
+    split_idx = int(np.floor(validation_fraction * len(data_train)))
+
+    val_indices = np.random.choice(indices, size=split_idx, replace=False)
+    train_indices = list(set(indices) - set(val_indices))
+
+    train_sampler = SubsetRandomSampler(train_indices)
+    validation_sampler = SubsetRandomSampler(val_indices)
+
+    dataloader_train = torch.utils.data.DataLoader(data_train,
+                                                   sampler=train_sampler,
+                                                   batch_size=batch_size,
+                                                   num_workers=2,
+                                                   drop_last=True)
+
+    dataloader_val = torch.utils.data.DataLoader(data_train,
+                                                 sampler=validation_sampler,
+                                                 batch_size=batch_size,
+                                                 num_workers=2)
+
+    dataloader_test = torch.utils.data.DataLoader(data_test,
+                                                  batch_size=batch_size,
+                                                  shuffle=False,
+                                                  num_workers=2)
+
+    return dataloader_train, dataloader_val, dataloader_test
