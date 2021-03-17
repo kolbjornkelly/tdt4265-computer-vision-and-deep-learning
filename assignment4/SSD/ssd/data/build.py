2660f6145f0ebaf6164d9e6074c34bbedb828740
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-03-17 22:34:06 +0100

    last try for the day

--- assignment4/SSD/ssd/data/build.py
+++ assignment4/SSD/ssd/data/build.py
@@ -68,10 +68,10 @@ def make_data_loader(cfg, is_train=True, augment=False, max_iter=None, start_ite
                                  pin_memory=cfg.DATA_LOADER.PIN_MEMORY, collate_fn=BatchCollator(is_train))
         data_loaders.append(data_loader)
 
-    data_loaders = ConcatDataset(data_loaders[0], data_loaders[1])
-    print("Dataloader length: ", len(data_loaders))
+    #data_loaders = ConcatDataset(data_loaders[0], data_loaders[1])
+    print("Dataloader length: ", len(data_loaders[1]))
     if is_train:
         # during training, a single (possibly concatenated) data_loader is returned
         assert len(data_loaders) == 1
-        return data_loaders
+        return data_loaders[0]
     return data_loaders
