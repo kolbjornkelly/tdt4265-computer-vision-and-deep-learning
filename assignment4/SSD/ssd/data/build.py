803712013f6922f710a2917ba86f43552ab5cfde
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-03-17 22:26:27 +0100

    skip loop entirely

--- assignment4/SSD/ssd/data/build.py
+++ assignment4/SSD/ssd/data/build.py
@@ -53,22 +53,21 @@ def make_data_loader(cfg, is_train=True, augment=False, max_iter=None, start_ite
 
     data_loaders = []
 
-    for dataset in datasets:
-        if shuffle:
-            sampler = torch.utils.data.RandomSampler(dataset)
-        else:
-            sampler = torch.utils.data.sampler.SequentialSampler(dataset)
+    if shuffle:
+        sampler = torch.utils.data.RandomSampler(datasets)
+    else:
+        sampler = torch.utils.data.sampler.SequentialSampler(datasets)
 
-        batch_size = cfg.SOLVER.BATCH_SIZE if is_train else cfg.TEST.BATCH_SIZE
-        batch_sampler = torch.utils.data.sampler.BatchSampler(
-            sampler=sampler, batch_size=batch_size, drop_last=is_train)
-        if max_iter is not None:
-            batch_sampler = samplers.IterationBasedBatchSampler(
-                batch_sampler, num_iterations=max_iter, start_iter=start_iter)
+    batch_size = cfg.SOLVER.BATCH_SIZE if is_train else cfg.TEST.BATCH_SIZE
+    batch_sampler = torch.utils.data.sampler.BatchSampler(
+        sampler=sampler, batch_size=batch_size, drop_last=is_train)
+    if max_iter is not None:
+        batch_sampler = samplers.IterationBasedBatchSampler(
+            batch_sampler, num_iterations=max_iter, start_iter=start_iter)
 
-        data_loader = DataLoader(dataset, num_workers=cfg.DATA_LOADER.NUM_WORKERS, batch_sampler=batch_sampler,
-                                 pin_memory=cfg.DATA_LOADER.PIN_MEMORY, collate_fn=BatchCollator(is_train))
-        data_loaders.append(data_loader)
+    data_loader = DataLoader(datasets, num_workers=cfg.DATA_LOADER.NUM_WORKERS, batch_sampler=batch_sampler,
+                             pin_memory=cfg.DATA_LOADER.PIN_MEMORY, collate_fn=BatchCollator(is_train))
+    data_loaders.append(data_loader)
 
     if is_train:
         # during training, a single (possibly concatenated) data_loader is returned
