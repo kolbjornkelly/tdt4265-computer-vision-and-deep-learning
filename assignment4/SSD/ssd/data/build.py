b9b3176d7c7791aedfc15f55651aad554163693d
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-03-17 22:31:30 +0100

    concat loaders

--- assignment4/SSD/ssd/data/build.py
+++ assignment4/SSD/ssd/data/build.py
@@ -49,28 +49,29 @@ def make_data_loader(cfg, is_train=True, augment=False, max_iter=None, start_ite
 
     shuffle = is_train
 
-    print("Datasets length: ", len(datasets))
-
     data_loaders = []
 
-    if shuffle:
-        sampler = torch.utils.data.RandomSampler(datasets)
-    else:
-        sampler = torch.utils.data.sampler.SequentialSampler(datasets)
+    for dataset in datasets:
+        if shuffle:
+            sampler = torch.utils.data.RandomSampler(dataset)
+        else:
+            sampler = torch.utils.data.sampler.SequentialSampler(dataset)
 
-    batch_size = cfg.SOLVER.BATCH_SIZE if is_train else cfg.TEST.BATCH_SIZE
-    batch_sampler = torch.utils.data.sampler.BatchSampler(
-        sampler=sampler, batch_size=batch_size, drop_last=is_train)
-    if max_iter is not None:
-        batch_sampler = samplers.IterationBasedBatchSampler(
-            batch_sampler, num_iterations=max_iter, start_iter=start_iter)
+        batch_size = cfg.SOLVER.BATCH_SIZE if is_train else cfg.TEST.BATCH_SIZE
+        batch_sampler = torch.utils.data.sampler.BatchSampler(
+            sampler=sampler, batch_size=batch_size, drop_last=is_train)
+        if max_iter is not None:
+            batch_sampler = samplers.IterationBasedBatchSampler(
+                batch_sampler, num_iterations=max_iter, start_iter=start_iter)
 
-    data_loader = DataLoader(datasets, num_workers=cfg.DATA_LOADER.NUM_WORKERS, batch_sampler=batch_sampler,
-                             pin_memory=cfg.DATA_LOADER.PIN_MEMORY, collate_fn=BatchCollator(is_train))
-    data_loaders.append(data_loader)
+        data_loader = DataLoader(dataset, num_workers=cfg.DATA_LOADER.NUM_WORKERS, batch_sampler=batch_sampler,
+                                 pin_memory=cfg.DATA_LOADER.PIN_MEMORY, collate_fn=BatchCollator(is_train))
+        data_loaders.append(data_loader)
 
+    data_loaders = ConcatDataset(data_loaders[0], data_loaders[1])
+    print("Dataloader length: ", len(data_loaders))
     if is_train:
         # during training, a single (possibly concatenated) data_loader is returned
-        #assert len(data_loaders) == 1
+        assert len(data_loaders) == 1
         return data_loaders
     return data_loaders
