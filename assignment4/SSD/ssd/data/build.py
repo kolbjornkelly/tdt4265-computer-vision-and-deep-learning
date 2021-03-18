5191da42a9e727eb2ca56ce05de635b634c27793
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-03-18 18:16:02 +0100

    debug

--- assignment4/SSD/ssd/data/build.py
+++ assignment4/SSD/ssd/data/build.py
@@ -36,7 +36,7 @@ def make_data_loader(cfg, is_train=True, augment=False, max_iter=None, start_ite
     if augment:
         augmentation_transform = build_transforms(
             cfg, is_train=is_train, augment=augment)
-        augmented_datasets = build_dataset(
+        datasets = build_dataset(
             cfg.DATASET_DIR,
             dataset_list, transform=augmentation_transform,
             target_transform=target_transform, is_train=is_train)
@@ -47,8 +47,6 @@ def make_data_loader(cfg, is_train=True, augment=False, max_iter=None, start_ite
             cfg.DATASET_DIR,
             dataset_list, transform=train_transform,
             target_transform=target_transform, is_train=is_train)
-
-    print("Dataset Length: ", len(datasets))
 
     shuffle = is_train
 
