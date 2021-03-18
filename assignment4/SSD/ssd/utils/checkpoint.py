11a12947408f302e4ff51a587ff7e344db1d5bd1
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-03-18 13:39:24 +0100

    control check

--- assignment4/SSD/ssd/utils/checkpoint.py
+++ assignment4/SSD/ssd/utils/checkpoint.py
@@ -5,7 +5,7 @@ from ssd.utils.model_zoo import cache_url
 
 
 class CheckPointer:
-    _last_checkpoint_name = 'last_checkpoint_augment_highLR.txt'
+    _last_checkpoint_name = 'last_checkpoint_augment_best_so_far.txt'
 
     def __init__(self,
                  model,
