76b8ab26de416ed8986ed90d74db6f1bed34cf98
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-03-23 16:06:47 +0100

    super low thresh

--- assignment4/SSD/ssd/config/defaults.py
+++ assignment4/SSD/ssd/config/defaults.py
@@ -87,11 +87,11 @@ cfg.SOLVER.WEIGHT_DECAY = 5e-4
 # ---------------------------------------------------------------------------- #
 cfg.TEST = CN()
 cfg.TEST.NMS_THRESHOLD = 0.45
-cfg.TEST.CONFIDENCE_THRESHOLD = 0.1  # 0.01
+cfg.TEST.CONFIDENCE_THRESHOLD = 0.001  # 0.01
 cfg.TEST.MAX_PER_CLASS = -1
 cfg.TEST.MAX_PER_IMAGE = 100
 cfg.TEST.BATCH_SIZE = 10
-cfg.EVAL_STEP = -1  # Evaluate dataset every eval_step, disabled when eval_step < 0
+cfg.EVAL_STEP = 500  # Evaluate dataset every eval_step, disabled when eval_step < 0
 cfg.MODEL_SAVE_STEP = 500  # Save checkpoint every save_step
 cfg.LOG_STEP = 10  # Print logs every log_stepPrint logs every log_step
 cfg.OUTPUT_DIR = "outputs"
