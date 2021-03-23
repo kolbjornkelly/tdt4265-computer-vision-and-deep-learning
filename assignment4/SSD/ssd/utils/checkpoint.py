71d430d052b43e205c464ed5a85e9f281ce0aa14
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-03-23 11:31:54 +0100

    remove pretrain

--- assignment4/SSD/ssd/utils/checkpoint.py
+++ assignment4/SSD/ssd/utils/checkpoint.py
@@ -5,7 +5,7 @@ from ssd.utils.model_zoo import cache_url
 
 
 class CheckPointer:
-    _last_checkpoint_name = 'pretrained.txt'
+    _last_checkpoint_name = 'nonpretrained.txt'
 
     def __init__(self,
                  model,
