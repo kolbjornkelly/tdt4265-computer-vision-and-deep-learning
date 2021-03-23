e9521994c5d8730f7dbb78597eaa741a9cda6f7f
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-03-23 14:32:16 +0100

    changed config

--- assignment4/SSD/ssd/config/defaults.py
+++ assignment4/SSD/ssd/config/defaults.py
@@ -28,13 +28,13 @@ cfg.MODEL.PRIORS = CN()
 cfg.MODEL.PRIORS.FEATURE_MAPS = [[77, 77], [38, 38], [
     19, 19], [10, 10], [5, 5], [3, 3], [1, 1]]
 # X, Y
-cfg.MODEL.PRIORS.STRIDES = [[4, 4], [8, 8], [16, 16], [
+cfg.MODEL.PRIORS.STRIDES = [[8, 8], [8, 8], [16, 16], [
     32, 32], [64, 64], [100, 100], [300, 300]]
 # X, Y
 cfg.MODEL.PRIORS.MIN_SIZES = [[15, 15], [30, 30], [60, 60], [
     111, 111], [162, 162], [213, 213], [264, 264]]
 # X, Y
-cfg.MODEL.PRIORS.MAX_SIZES = [[30, 30], [60, 60], [111, 111], [
+cfg.MODEL.PRIORS.MAX_SIZES = [[60, 60], [60, 60], [111, 111], [
     162, 162], [213, 213], [264, 264], [315, 315]]
 cfg.MODEL.PRIORS.ASPECT_RATIOS = [[2], [2], [2, 3], [2, 3], [2, 3], [2], [2]]
 # When has 1 aspect ratio, every location has 4 boxes, 2 ratio 6 boxes.
