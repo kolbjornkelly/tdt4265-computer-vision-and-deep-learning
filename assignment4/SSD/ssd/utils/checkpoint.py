e3311f870906b64e542693589a4272567781793c
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-03-23 16:32:14 +0100

    new path

--- assignment4/SSD/ssd/utils/checkpoint.py
+++ assignment4/SSD/ssd/utils/checkpoint.py
@@ -5,7 +5,7 @@ from ssd.utils.model_zoo import cache_url
 
 
 class CheckPointer:
-    _last_checkpoint_name = 'no_ex_layer1.txt'
+    _last_checkpoint_name = 'increase_layers.txt'
 
     def __init__(self,
                  model,
