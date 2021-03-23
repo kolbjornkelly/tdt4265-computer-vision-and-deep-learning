3966d3ade5a32af64cdb32ff5bb04f1af4245050
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-03-23 19:39:53 +0100

    clean up

--- assignment4/SSD/ssd/modeling/detector.py
+++ assignment4/SSD/ssd/modeling/detector.py
@@ -1,6 +1,6 @@
 from torch import nn
 from ssd.modeling.backbone.vgg import VGG
-from ssd.modeling.backbone.improved_basic import BasicModel
+from ssd.modeling.backbone.final_model import BasicModel
 from ssd.modeling.box_head.box_head import SSDBoxHead
 from ssd.utils.model_zoo import load_state_dict_from_url
 from ssd import torch_utils
@@ -30,7 +30,7 @@ class SSDDetector(nn.Module):
 
 def build_backbone(cfg):
     backbone_name = cfg.MODEL.BACKBONE.NAME
-    if backbone_name == "improved_basic":
+    if backbone_name == "final_model":
         model = BasicModel(cfg)
         return model
     if backbone_name == "vgg":
