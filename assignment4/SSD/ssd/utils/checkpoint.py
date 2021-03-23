920f229f3901d2c27e1577a04d8c5c4d00258b4a
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-03-23 18:45:33 +0100

    add path

--- assignment4/SSD/ssd/utils/checkpoint.py
+++ assignment4/SSD/ssd/utils/checkpoint.py
@@ -5,7 +5,7 @@ from ssd.utils.model_zoo import cache_url
 
 
 class CheckPointer:
-    _last_checkpoint_name = 'first_try4d.txt'
+    _last_checkpoint_name = 'sec_try4d.txt'
 
     def __init__(self,
                  model,
