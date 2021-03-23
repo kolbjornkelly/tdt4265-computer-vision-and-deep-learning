45fc25ceca37a3e6a5496c78d392f6551c336fb7
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-03-23 09:47:38 +0100

    new checkpoint

--- assignment4/SSD/ssd/utils/checkpoint.py
+++ assignment4/SSD/ssd/utils/checkpoint.py
@@ -5,7 +5,7 @@ from ssd.utils.model_zoo import cache_url
 
 
 class CheckPointer:
-    _last_checkpoint_name = 'scheduler_larger_network.txt'
+    _last_checkpoint_name = 'scheduler_tuned.txt'
 
     def __init__(self,
                  model,
