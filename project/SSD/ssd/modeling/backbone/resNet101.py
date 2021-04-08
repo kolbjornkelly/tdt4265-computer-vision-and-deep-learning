4cb9c3feca5cee00c7a4d0588165458203f73297
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-04-08 16:33:16 +0200

    reduce #feature layers

--- project/SSD/ssd/modeling/backbone/resNet101.py
+++ project/SSD/ssd/modeling/backbone/resNet101.py
@@ -20,14 +20,31 @@ class ResNet101(nn.Module):
 
         # TODO: Add documentation
         out_features = []
+        """
         # Pass through first layer
         out_features.append(self.model.conv1(x))
         # Define remaining layers
         layers = nn.Sequential(*(list(self.model.children())[1:9]))
 
         # Pass through remaining layers
+        idx_counter = 0
+        layers_to_use = []
         for layer in layers:
+            if idx_counter == 
             out_features.append(layer(out_features[-1]))
+        """
+        x = self.model.conv1(x)
+        idx_counter = 0
+        layers_to_use = [2,4,5,6,7,8]
+
+        layers = nn.Sequential(*(list(self.model.children())[1:9]))
+
+        for layer in layers:
+            x = layer(x)
+            if idx_counter in layers_to_use:
+                out_features.append(x)
+            idx_counter += 1
+
 
         
         # Ensure correct shapes
