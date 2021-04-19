b38c4c8547f867d29f6884e6bdb4bcfe283d7555
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-04-19 09:18:52 +0200

    unfreeze resnet18

--- project/SSD/ssd/modeling/backbone/resNet18.py
+++ project/SSD/ssd/modeling/backbone/resNet18.py
@@ -16,16 +16,16 @@ class ResNet18(nn.Module):
 
         #self.model.fc = nn.Linear(512, 10)  # No need to apply softmax,
         # as this is done in nn.CrossEntropyLoss
-
+        """
         for param in self.model.parameters():  # Freeze all parameters
             param.requires_grad = False
-        """
+        
         for param in self.model.fc.parameters():  # Unfreeze the last fully-connected
             param.requires_grad = True  # layer
-        """
+        
         for param in self.model.layer4.parameters():  # Unfreeze the last 5 convolutional
             param.requires_grad = True  # layers
-
+        """
 
     def forward(self, x):
         # TODO: Add documentation
